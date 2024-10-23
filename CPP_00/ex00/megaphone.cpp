ff57cbe159dcca31b24086ffcc52febbf471dd53
ilies1511 <iziane@student.42heilbronn.de>
2024-10-23 19:08:58 +0200

    FEAT(megaphone): Add ex00

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <cctype>
 
-int	sum(int a, int b)
+std::string str_toupper(std::string str)
 {
-	return(a + b);
+	std::transform(str.begin(), str.end(), str.begin(),
+		[](unsigned char c){ return std::toupper(c);});
+	return (str);
+}
+
+static void	megaphone(std::string str)
+{
+	std:: cout << str_toupper(str);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		megaphone("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+	else
+	{
+		for (int i = 1; i < (argc); i++)
+			megaphone(argv[i]);
+	}
+	std::cout << '\n';
+	return (0);
 }
