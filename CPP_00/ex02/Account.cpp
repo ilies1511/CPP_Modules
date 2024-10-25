cc209ab43f34b5dc6e079d817dfd95fd15d00a4a
ilies1511 <iziane@student.42heilbronn.de>
2024-10-26 00:25:35 +0200

    FEAT(getters): Add easy getter functions

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -1,2 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 00:06:49 by iziane            #+#    #+#             */
+/*   Updated: 2024/10/26 00:23:25 by iziane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Account.hpp"
+
+int	Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	;
+}
 
