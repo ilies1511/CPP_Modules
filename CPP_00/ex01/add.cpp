da626e57f771b48a7e7d77304ff3d439f100e6e8
ilies1511 <iziane@student.42heilbronn.de>
2024-10-25 19:34:05 +0200

    REFACTOR(ex01, ADD): using template fnc to store contact data

--- CPP_00/ex01/add.cpp
+++ CPP_00/ex01/add.cpp
@@ -6,23 +6,6 @@ void	add_darkest_secret(Contact *new_contact)
 	std::string	str;
 	Log			status;
 
-	// while (1)
-	// {
-	// 	std::cout << "Enter darkest secret: ";
-	// 	std::getline(std::cin, str);
-	// 	std::stringstream ss(str);
-	// 	if (ss >> action)
-	// 	{
-	// 		new_contact->add_x(action, new_contact->DARKEST_SECRET);
-	// 		new_contact->get_x((new_contact->DARKEST_SECRET));
-	// 	}
-	// 	else
-	// 	{
-	// 		status.mf_set_level(ERROR);
-	// 		status.mf_error("Please enter valid input");
-	// 	}
-	// 	// std::system("clear");
-	// }
 	while (1)
 	{
 		std::cout << "Enter darkest secret: ";
@@ -34,13 +17,10 @@ void	add_darkest_secret(Contact *new_contact)
 			status.mf_error("Please enter valid input...)");
 			continue ;
 		}
-		// ss >> action;
-		// ss >> action;
 		new_contact->add_x(str, new_contact->DARKEST_SECRET);
 		// new_contact->add_x(action, new_contact->DARKEST_SECRET);
 		new_contact->get_x((new_contact->DARKEST_SECRET));
 		break;
-		// std::system("clear");
 	}
 }
 
@@ -91,29 +71,7 @@ void	add_phone_nbr(Contact *new_contact)
 	}
 }
 
-void	add_nickname(Contact *new_contact)
-{
-	std::string	action;
-	std::string	str;
-	Log			status;
-
-	while (1)
-	{
-		std::cout << "Enter nickname: ";
-		std::getline(std::cin, str);
-		std::stringstream ss(str);
-		if (ss >> action && !(ss >> action))
-		{
-			new_contact->add_x(action, new_contact->NICKNAME);
-			new_contact->get_x((new_contact->NICKNAME));
-			break ;
-		}
-		status.mf_set_level(ERROR);
-		status.mf_error("Please enter valid Nickname (Only name)");
-	}
-}
-
-void	add_last_name(Contact *new_contact)
+void	add_template(Contact *new_contact, Contact::Add type, std::string input)
 {
 	std::string	action;
 	std::string	str;
@@ -126,47 +84,19 @@ void	add_last_name(Contact *new_contact)
 			std::system("clear");
 			exit(1);
 		}
-		std::cout << "Enter last name: ";
+		std::cout << "Enter " << input << ": ";
 		std::getline(std::cin, str);
 		std::stringstream ss(str);
 		if (ss >> action && !(ss >> action))
 		{
-			new_contact->add_x(action, new_contact->LAST_NAME);
-			new_contact->get_x((new_contact->LAST_NAME));
-			break ;
-		}
-		else
-			status.mf_set_level(ERROR);
-			status.mf_error("Please enter valid Last Name (Only name)");
-	}
-}
-
-void	add_first_name(Contact *new_contact)
-{
-	std::string	action;
-	std::string	str;
-	Log			status;
-
-	while (1)
-	{
-		if (std::cin.eof())
-		{
-			std::system("clear");
-			exit(1);
-		}
-		std::cout << "Enter first name: ";
-		std::getline(std::cin, str);
-		std::stringstream ss(str);
-		if (ss >> action && !(ss >> action))
-		{
-			new_contact->add_x(action, new_contact->FIRST_NAME);
-			new_contact->get_x((new_contact->FIRST_NAME));
+			new_contact->add_x(action, type);
+			new_contact->get_x((type));
 			break ;
 		}
 		else
 		{
 			status.mf_set_level(ERROR);
-			status.mf_error("Please enter valid Name (Enter one name)");
+			status.mf_error("Please enter valid " + input);
 		}
 	}
 }
@@ -176,9 +106,10 @@ void	add_contact(PhoneBook *pb)
 	Contact		new_contact;
 
 	std::cout << "PRE ADD Operation: " << pb->amount_contacts << '\n';
-	add_first_name(&new_contact);
-	add_last_name(&new_contact);
-	add_nickname(&new_contact);
+	// add_first_name(&new_contact);
+	add_template(&new_contact, Contact::FIRST_NAME, "FIRST_NAME");
+	add_template(&new_contact, Contact::LAST_NAME, "LAST NAME");
+	add_template(&new_contact, Contact::NICKNAME, "NICK_NAME");
 	add_phone_nbr(&new_contact);
 	add_darkest_secret(&new_contact);
 	pb->add_contact_to_pb(new_contact);
