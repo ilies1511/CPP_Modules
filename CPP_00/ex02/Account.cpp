06deefea52bf77a41312148c6e61083ab7afaa3d
ilies1511 <iziane@student.42heilbronn.de>
2024-10-26 20:49:36 +0200

    FIX(ex02): Fix constructor and go from -std=c++17 to -std=c++98 to avoid compilation error

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -6,11 +6,19 @@
 /*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 00:06:49 by iziane            #+#    #+#             */
-/*   Updated: 2024/10/26 19:33:44 by iziane           ###   ########.fr       */
+/*   Updated: 2024/10/26 20:45:12 by iziane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+
+/*
+	Comparing *.log file outputs:
+		1. make re && ./account > final
+		2. awk '{$1=""}1' final > final_cut
+		3. awk '{$1=""}1' 19920104_091532.log > og_cut
+		3. diff og_cut final_cut
+*/
 
 int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
@@ -43,23 +51,25 @@ void	Account::_displayTimestamp(void)
 	std::tm localTime;
 
 	localtime_r(&now, &localTime);
-	std::cout << std::put_time(&localTime, "[%Y%m%d_%H%M%S]") << std::endl;
+	std::cout << std::put_time(&localTime, "[%Y%m%d_%H%M%S]");
 }
 
 void	Account::displayAccountsInfos(void)
 {
 	_displayTimestamp();
-	std::cout << " accounts:" << getNbAccounts() << ";total:" << getTotalAmount() \
-		<< ";deposits:" << getNbDeposits() \
-			<< ";withdrawals:" << getNbWithdrawals() << std::endl;
+	std::cout << " accounts:" << _nbAccounts << ";total:" << _totalAmount \
+		<< ";deposits:" << _totalNbDeposits \
+			<< ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 Account::Account(int initial_doposit)
 {
 	_accountIndex = getNbAccounts();
 	_amount = initial_doposit;
+	_totalAmount = _totalAmount + _amount;
 	_nbDeposits = 0;
 	_nbWithdrawals = 0;
+	_nbAccounts++;
 	_displayTimestamp();
 	std::cout << " index:" << _accountIndex << ";amount:" << _amount \
 		<< ";created" << std::endl;
