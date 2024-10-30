2cf8ae3fe48de43dc61a65a31053fa6d404960e1
ilies1511 <iziane@student.42heilbronn.de>
2024-10-30 16:38:41 +0100

    FEAT(cpp01, ex02): add finished version

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+
+void	print_values(std::string brain, std::string *stringPTR, std::string &stringREF)
+{
+	const int label_width = 40;
+
+	std::cout << std::setw(label_width) << "value held by 'brain': " << brain << '\n';
+	std::cout << std::setw(label_width) << "value pointed to by 'stringPTR': " << *stringPTR << '\n';
+	std::cout << std::setw(label_width) << "value pointed to by 'stringREF': " << stringREF << '\n';
+	return ;
+}
+
+void	print_memory_address(std::string *brain, std::string *stringPTR, std::string &stringREF)
+{
+	const int label_width = 40;
+
+	std::cout << std::left;
+	// memory adress of string;
+	std::cout << std::setw(label_width) << "memory adress of string: " << brain << "\n";
+	// memory adress held by 'stringPTR';
+	std::cout << std::setw(label_width) << "memory adress held by stringPTR: " << stringPTR << "\n";
+	// memory adress held by 'stringREF';
+	std::cout << std::setw(label_width) << "memory adress held by stringREF: " << &stringREF << "\n";
+}
 
 int	main(void)
 {
-	std::string brain = "HI THIS IS BRAIN";
+	std::string brain; //String 'brain'
+	std::string *stringPTR = &brain; //Pointer to brain
+	std::string &stringREF = brain; //Reference to 'brain'
 
-	std::cout << "Ouey Zizou\n";
+	brain = "HI THIS IS BRAIN";
+	print_memory_address(&brain, stringPTR, stringREF);
+	print_values(brain, stringPTR, stringREF);
 	return (0);
 }
