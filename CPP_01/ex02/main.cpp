2de3389e2278d030b09132f0d83c90c5ab791fd1
ilies1511 <iziane@student.42heilbronn.de>
2024-10-30 22:28:46 +0100

    STYLE(CPP01, ex02): print_mem_add(): go from 'std::string *brain' --> 'const std::string& brain'

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -12,13 +12,13 @@ void	print_values(std::string brain, std::string *stringPTR, std::string &string
 	return ;
 }
 
-void	print_memory_address(std::string *brain, std::string *stringPTR, std::string &stringREF)
+void	print_memory_address(const std::string& brain, std::string *stringPTR, std::string &stringREF)
 {
 	const int label_width = 40;
 
 	std::cout << std::left;
 	// memory adress of string;
-	std::cout << std::setw(label_width) << "memory adress of string: " << brain << "\n";
+	std::cout << std::setw(label_width) << "memory adress of string: " << &brain << "\n";
 	// memory adress held by 'stringPTR';
 	std::cout << std::setw(label_width) << "memory adress held by stringPTR: " << stringPTR << "\n";
 	// memory adress held by 'stringREF';
@@ -32,7 +32,7 @@ int	main(void)
 	std::string &stringREF = brain; //Reference to 'brain'
 
 	brain = "HI THIS IS BRAIN";
-	print_memory_address(&brain, stringPTR, stringREF);
+	print_memory_address(brain, stringPTR, stringREF);
 	print_values(brain, stringPTR, stringREF);
 	return (0);
 }
