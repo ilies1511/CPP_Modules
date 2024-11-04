46bb9c47a13507fa08ad2ecb39b47078d2b217fb
ilies1511 <iziane@student.42heilbronn.de>
2024-11-04 13:37:08 +0100

    FEAT(CPP02, ex00): add methode implementation

--- CPP_02/ex00/src/Fixed.cpp
+++ CPP_02/ex00/src/Fixed.cpp
@@ -1,11 +1,10 @@
 #include "Fixed.hpp"
 
+// Orthodox Canonical Form (Design) - Begin
 //Default Constructor
 FixPointNbr::FixPointNbr() : _fix_point_value(0){}
-
 //Copy Constructor
 FixPointNbr::FixPointNbr(const FixPointNbr& og) : _fix_point_value(og._fix_point_value){}
-
 //Copy assignment operator
 FixPointNbr &FixPointNbr::operator=(const FixPointNbr& og)
 {
@@ -15,6 +14,18 @@ FixPointNbr &FixPointNbr::operator=(const FixPointNbr& og)
 	}
 	return (*this);
 }
-
 //Destructor
 FixPointNbr::~FixPointNbr(){};
+// Orthodox Canonical Form (Design) - End
+
+//Methodes Implementation - Begin
+int FixPointNbr::getRawBits( void ) const
+{
+	return (_fix_point_value);
+}
+
+void FixPointNbr::setRawBits( int const raw )
+{
+	_fix_point_value = raw;
+}
+//Methodes Implementation - End
