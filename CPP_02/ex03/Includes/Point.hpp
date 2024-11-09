0bbe47c663a0cb47356f0e9a1d0b5799495c0a9c
ilies1511 <iziane@student.42heilbronn.de>
2024-11-09 19:58:09 +0100

    FEAT(cpp02, ex03): add getters for coordinates & coloring fnc

--- CPP_02/ex03/Includes/Point.hpp
+++ CPP_02/ex03/Includes/Point.hpp
@@ -4,6 +4,15 @@
 //Includes
 #include "Fixed.hpp"
 
+// ANSI Escape Codes
+const std::string GREEN = "\033[0;32m";
+const std::string RED = "\033[0;31m";
+const std::string BLUE = "\033[0;34m";
+const std::string BOLD = "\033[1m";
+const std::string ORANGE = "\033[38;2;255;165;0m";
+const std::string PURPLE = "\033[38;2;128;0;128m";
+const std::string NC = "\033[0m"; // Zurücksetzen
+
 class Point
 {
 	private:
@@ -19,12 +28,25 @@ class Point
 		Point(const Point &input);
 		//Constructor with const float as param
 		//Copy assignment operator overload
+		/*
+			Da Point nur const-Mitglieder enthält, ist die Klasse im
+			Wesentlichen unveränderlich (immutable), was bedeutet,
+			dass ein Zuweisungsoperator nicht benötigt
+		*/
 		Point &operator=(Point& og);
 		//Destructor
 		~Point();
+	//Members:
+		const Fixed& getX() const;
+		const Fixed& getY() const;
 };
 
 //FNC-Prototypes
 bool bsp( Point const a, Point const b, Point const c, Point const point);
+/*
+	Usage:
+		std::cout << coloring("Dieser Text ist grün!", GREEN) << std::endl;
+*/
+std::string coloring(const std::string text, const std::string &color);
 
 #endif
--- CPP_02/ex03/src/Point.cpp
+++ CPP_02/ex03/src/Point.cpp
@@ -6,6 +6,12 @@ Point::Point(const float input1, const float input2) : x(input1), y(input2){};
 
 Point::Point(const Point& og) : x(og.x), y(og.y){}
 
+//Copy assignment operator overload
+/*
+	Da Point nur const-Mitglieder enthält, ist die Klasse im
+	Wesentlichen unveränderlich (immutable), was bedeutet,
+	dass ein Zuweisungsoperator nicht benötigt
+*/
 Point &Point::operator=(Point& og)
 {
 	if (this != &og)
@@ -15,6 +21,20 @@ Point &Point::operator=(Point& og)
 
 Point::~Point(){};
 
+//Mebers
 
+const Fixed& Point::getX() const
+{
+	return (x);
+}
 
+const Fixed& Point::getY() const
+{
+	return (y);
+}
 
+//Display
+std::string coloring(const std::string text, const std::string &color)
+{
+	return (color + text + NC);
+}
