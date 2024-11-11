9c3a78eec5eeb5412a6b55d3e544bc8dcfeadacd
ilies1511 <iziane@student.42heilbronn.de>
2024-11-11 01:02:29 +0100

    TEST(cpp03, ex00): add test2

--- CPP_03/ex00/src/main.cpp
+++ CPP_03/ex00/src/main.cpp
@@ -1,8 +1,29 @@
 #include "ClapTrap.hpp"
 
+void	test3(ClapTrap &Blayer1, ClapTrap &Blayer2, ClapTrap &Blayer3)
+{
+
+}
+
+void	test2(ClapTrap &Blayer1, ClapTrap &Blayer2, ClapTrap &Blayer3)
+{
+	(void)Blayer3;
+	std::cout << coloring("\nTEST2\n", RED);
+	Blayer2.beRepaired(30);
+	while (1)
+	{
+		if (Blayer2.getHitPoint() <= 0 || Blayer1.getEnergyPoint() <= 0)
+			break ;
+		Blayer1.attack(Blayer2.getName());
+		Blayer2.takeDamage(static_cast<unsigned int>(Blayer1.getAttackDamage()));
+		Blayer1.setAttackDamage(Blayer1.getAttackDamage() + 1);
+	}
+}
+
 void	test1(ClapTrap &Blayer1, ClapTrap &Blayer2, ClapTrap &Blayer3)
 {
 	(void)Blayer3;
+	std::cout << coloring("\nTEST1\n", RED);
 	Blayer1.setAttackDamage(6);
 	Blayer2.setAttackDamage(6);
 	std::cout << "Hit Point PRE " << Blayer2.getHitPoint() << "\n";
@@ -19,7 +40,7 @@ int	main()
 	ClapTrap	Blayer3;
 
 	test1(Blayer1, Blayer2, Blayer3);
-
-	std::cout << "Alo\n";
+	test2(Blayer1, Blayer2, Blayer3);
+	test3();
 	return (0);
 }
