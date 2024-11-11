8bc227e18e44ce29186579887d98993d65f67223
ilies1511 <iziane@student.42heilbronn.de>
2024-11-11 12:18:29 +0100

    INIT(cpp03, ex01): initial commit"

---
+++ CPP_03/ex01/Includes/ClapTrap.hpp
@@ -0,0 +1,62 @@
+#ifndef CLAP_TRAP_HPP
+# define CLAP_TRAP_HPP
+
+//Includes
+#include <iostream>
+#include <string>
+
+// ANSI Escape Codes
+const std::string GREEN = "\033[0;32m";
+const std::string RED = "\033[0;31m";
+const std::string BLUE = "\033[0;34m";
+const std::string BOLD = "\033[1m";
+const std::string ORANGE = "\033[38;2;255;165;0m";
+const std::string PURPLE = "\033[38;2;128;0;128m";
+const std::string NC = "\033[0m"; // Zurücksetzen
+
+//Class
+class ClapTrap
+{
+	private:
+		std::string _name;
+		//represent the health of the ClapTrap
+		size_t		_HitPoint;
+		//attacking and repairing cost 1 energy point each
+		size_t		_EnergyPoint;
+		size_t		_AttackDamage;
+	//Orthodox Canonical Form
+	public:
+		//Default Contructor
+		ClapTrap();
+		//Default Name Constructor
+		ClapTrap(std::string InputName);
+		//Copy Constructor
+		ClapTrap(const ClapTrap &og);
+		//Coopy Assignment operator
+		ClapTrap& operator=(const ClapTrap& og);
+		//Destructor
+		~ClapTrap();
+	//Members
+	public:
+		void		attack(const std::string& target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+	//getter/setter
+	public:
+		//setter
+			// void		setName(void);
+		void setAttackDamage(size_t const damage);
+		//getter
+		std::string	getName(void) const;
+		size_t		getHitPoint(void) const;
+		size_t		getEnergyPoint(void) const;
+		size_t		getAttackDamage(void) const;
+};
+
+//FNC-Prototyp
+	/*
+		Usage:
+			std::cout << coloring("Dieser Text ist grün!", GREEN) << std::endl;
+	*/
+	std::string coloring(const std::string text, const std::string &color);
+#endif
---
+++ CPP_03/ex01/src/main.cpp
@@ -0,0 +1,50 @@
+#include "ClapTrap.hpp"
+
+void	test3(ClapTrap &Blayer1, ClapTrap &Blayer3)
+{
+	std::cout << coloring("\nTEST3\n", RED);
+	Blayer3.setAttackDamage(10000);
+	Blayer3.attack(Blayer1.getName());
+	Blayer1.takeDamage(static_cast<unsigned int>(Blayer3.getAttackDamage()));
+}
+
+void	test2(ClapTrap &Blayer1, ClapTrap &Blayer2, ClapTrap &Blayer3)
+{
+	(void)Blayer3;
+	std::cout << coloring("\nTEST2\n", RED);
+	Blayer2.beRepaired(30);
+	Blayer1.beRepaired(50);
+	while (1)
+	{
+		if (Blayer2.getHitPoint() <= 0 || Blayer1.getEnergyPoint() <= 0)
+			break ;
+		Blayer1.attack(Blayer2.getName());
+		Blayer2.takeDamage(static_cast<unsigned int>(Blayer1.getAttackDamage()));
+		Blayer1.setAttackDamage(Blayer1.getAttackDamage() + 1);
+	}
+}
+
+void	test1(ClapTrap &Blayer1, ClapTrap &Blayer2, ClapTrap &Blayer3)
+{
+	(void)Blayer3;
+	std::cout << coloring("\nTEST1\n", RED);
+	Blayer1.setAttackDamage(6);
+	Blayer2.setAttackDamage(6);
+	std::cout << "Hit Point PRE " << Blayer2.getHitPoint() << "\n";
+	Blayer1.attack(Blayer2.getName());
+	Blayer2.takeDamage(static_cast<unsigned int>(Blayer2.getAttackDamage()));
+	Blayer2.beRepaired(5);
+	std::cout << "Hit Point POST " << Blayer2.getHitPoint() << "\n";
+}
+
+int	main()
+{
+	ClapTrap	Blayer1("Zizou");
+	ClapTrap	Blayer2("Beter");
+	ClapTrap	Blayer3;
+
+	test1(Blayer1, Blayer2, Blayer3);
+	test2(Blayer1, Blayer2, Blayer3);
+	test3(Blayer1, Blayer3);
+	return (0);
+}
