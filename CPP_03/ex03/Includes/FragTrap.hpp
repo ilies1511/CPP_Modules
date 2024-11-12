e974db584e082ae7b25b251a7187b0bdea783631
ilies1511 <iziane@student.42heilbronn.de>
2024-11-12 22:16:52 +0100

    INIT(cpp03, ex03): initial commit

---
+++ CPP_03/ex03/Includes/FragTrap.hpp
@@ -0,0 +1,22 @@
+#ifndef FRAG_TRAP_HPP
+# define FRAG_TRAP_HPP
+
+//Includes
+# include "ClapTrap.hpp"
+//Classes
+
+class FragTrap : public ClapTrap
+{
+	//OCF
+	public:
+		FragTrap();
+		explicit FragTrap(std::string InputName);
+		FragTrap(const FragTrap &og);
+		FragTrap& operator=(const FragTrap &og);
+		~FragTrap();
+	//Memebers
+	public:
+		void highFivesGuys(void);
+};
+
+#endif
---
+++ CPP_03/ex03/src/FragTrap.cpp
@@ -0,0 +1,47 @@
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : ClapTrap()
+{
+	_HitPoint = 100;
+	_EnergyPoint = 100;
+	_AttackDamage = 30;
+	std::cout << "TrapTrap default constructor called\n";
+}
+
+FragTrap::FragTrap(std::string InputName) : ClapTrap()
+{
+	_HitPoint = 100;
+	_EnergyPoint = 100;
+	_AttackDamage = 30;
+	std::cout << "TrapTrap named constructor called for " << InputName << "\n";
+}
+
+FragTrap::FragTrap(const FragTrap &og) : ClapTrap(og)
+{
+	*this = og;
+	std::cout << "FragTrap copy constructor called\n";
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &og)
+{
+	std::cout << "FragTrap assignment operator called\n";
+	if (this != &og)
+	{
+		ClapTrap::operator=(og); //Ueberfluessig ?
+		_HitPoint = og._HitPoint;
+		_EnergyPoint = og._EnergyPoint;
+		_AttackDamage = og._AttackDamage;
+	}
+	return (*this);
+}
+
+// Destruktor
+FragTrap::~FragTrap()
+{
+	std::cout << "FragTrap destructor called\n";
+}
+
+void FragTrap::highFivesGuys(void)
+{
+	std::cout << "FragTrag " << this->_name << " requests a high five!\n";
+}
