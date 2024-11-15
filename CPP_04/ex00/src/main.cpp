77547599837db37243f0221fbed8cc5b1cad2283
ilies1511 <iziane@student.42heilbronn.de>
2024-11-15 20:43:54 +0100

    TEST(cpp04, ex00): add test

--- CPP_04/ex00/src/main.cpp
+++ CPP_04/ex00/src/main.cpp
@@ -3,30 +3,40 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 
+/*
+	Leaks Check:
+		dorker valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./ex00.out
+*/
+
 int main(void)
 {
-	// const Animal* meta = new Animal();
-	// const Animal* j = new Dog();
-	// const Animal* i = new Cat();
-	// std::cout << j->getType() << " " << std::endl;
-	// std::cout << i->getType() << " " << std::endl;
-	// i->makeSound(); //will output the cat sound!
-	// j->makeSound();
-	// meta->makeSound();
-	// //And more own tests
-	// std::cout << "Alo\n";
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
 
-	// // Animal hayuan;
-	// Animal hayuan2("Musti");
+	delete meta;
+	delete j;
+	delete i;
+	// // //And more own tests
+	// // std::cout << "Alo\n";
 
-	// // hayuan.makeSound();
-	// // hayuan2.makeS.ound();
-	Cat	cat;
-	cat.makeSound();
+	// // // Animal hayuan;
+	// // Animal hayuan2("Musti");
 
-	std::cout << "\n";
-	Dog dog;
-	dog.makeSound();
+	// // // hayuan.makeSound();
+	// // // hayuan2.makeS.ound();
+
+	// Cat	cat;
+	// cat.makeSound();
+
+	// std::cout << "\n";
+	// Dog dog;
+	// dog.makeSound();
 
 	return (0);
 }
