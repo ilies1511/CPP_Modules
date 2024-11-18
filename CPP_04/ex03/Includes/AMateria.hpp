8d88ed6be3af14c2de78756e8edaabcc1be32e09
ilies1511 <iziane@student.42heilbronn.de>
2024-11-18 23:16:19 +0100

    CHORE(cpp04, ex03): add AMateria.hpp

--- CPP_04/ex03/Includes/AMateria.hpp
+++ CPP_04/ex03/Includes/AMateria.hpp
@@ -7,11 +7,17 @@
 class AMateria
 {
 	protected:
-
+		std::string _type; //Either ice or cure
+	//OCF
 	public:
+		AMateria(void);
 		AMateria(std::string const & type);
+		AMateria(const AMateria &og);
+		AMateria& operator=(const AMateria &og);
+		virtual ~AMateria();
+	//Methodes
+	public:
 		std::string const & getType() const; //Returns the materia type
 		virtual AMateria* clone() const = 0;
-		virtual void use(ICharacter& target);
 	};
 #endif
