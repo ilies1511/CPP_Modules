eba0e5c4998ef4b1fc13f4bbd9e988cf86073478
ilies1511 <iziane@student.42heilbronn.de>
2024-11-20 20:37:53 +0100

    CHORES(cpp04, ex03): clean up

--- CPP_04/ex03/src/main.cpp
+++ CPP_04/ex03/src/main.cpp
@@ -4,49 +4,7 @@
 #include "Character.hpp"
 #include "Ice.hpp"
 #include "Cure.hpp"
-#include "extra.hpp"
-
-namespace printer
-{
-	enum class MessageType { INFO, PROMPT, ERROR, HEADER };
-
-	//Usage: printer::PrintMessage("Memory allocation failed for " + header, printer::MessageType::ERROR);
-	void PrintMessage(const std::string &message, MessageType type = MessageType::INFO)
-	{
-		std::string color;
-		switch (type)
-		{
-			case MessageType::INFO:		color = PURPLE;	break;
-			case MessageType::PROMPT:	color = BLUE;	break;
-			case MessageType::ERROR:	color = RED;	break;
-			case MessageType::HEADER:	color = ORANGE;	break;
-		}
-		std::cout << coloring(message, color) << "\n";
-	}
-
-	void	Header(const std::string &Input)
-	{
-		std::cout << coloring(Input, ORANGE) << "\n";
-	}
-
-	void	print_Interactive(const std::string &Input)
-	{
-		std::cout << coloring(Input, BLUE);
-	}
-
-	void	print_invalidInput()
-	{
-		std::cout << \
-			coloring("[INFO]: Invalid Command. USAGE: <cat> <dog> <wronganimal> <wrongcat> <clear> <exit>", RED) << "\n";
-	}
-
-	void	print_floor(Floor	*sharedFloor)
-	{
-		for (size_t i = 0; i < (sharedFloor->getFloorSize()); i++)
-			std::cout << "Item[" << i << "]: " << sharedFloor->getMateria(i)->getType() << "\n";
-	}
-}
-
+#include "printer.hpp"
 
 /// @brief
 /// @param
