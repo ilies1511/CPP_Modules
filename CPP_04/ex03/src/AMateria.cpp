96ab9f0734653dafa0ffcb197fbe7ddfdd51b2bb
ilies1511 <iziane@student.42heilbronn.de>
2024-11-20 23:38:39 +0100

    CHORE(cpp04, ex03): apply ocf printer

--- CPP_04/ex03/src/AMateria.cpp
+++ CPP_04/ex03/src/AMateria.cpp
@@ -1,31 +1,38 @@
 #include "AMateria.hpp"
+#include "printer.hpp"
 
 AMateria::AMateria(void) : _type("DefaultMateria")
 {
 	std::cout << "[AMateria]: Default Constructor called\n";
+	printer::ocf_printer("AMateria", printer::OCF_TYPE::DC, OLIVE);
 }
 
 AMateria::AMateria(std::string const & type) : _type(type)
 {
-	std::cout << "[AMateria]: Default Name Constructor called\n";
+	// std::cout << "[AMateria]: Default Name Constructor called\n";
+	printer::ocf_printer("AMateria", printer::OCF_TYPE::DNC, OLIVE);
 }
 
 AMateria::AMateria(const AMateria &og)
 {
-	std::cout << "[AMateria]: Copy Constructor called\n";
+	// std::cout << "[AMateria]: Copy Constructor called\n";
+	printer::ocf_printer("AMateria", printer::OCF_TYPE::CC, OLIVE);
+
 	this->_type = og._type;
 }
 
 AMateria &AMateria::operator=(const AMateria &og)
 {
-	std::cout << "[AMateria]: Copy Assigmen Constructor called\n";
+	// std::cout << "[AMateria]: Copy Assigmen Constructor called\n";
+	printer::ocf_printer("AMateria", printer::OCF_TYPE::CAC, OLIVE);
 	(void)og;
 	return (*this);
 }
 
 AMateria::~AMateria()
 {
-	std::cout << "[AMateria]: Destructor called\n";
+	printer::ocf_printer("AMateria", printer::OCF_TYPE::D, OLIVE);
+	// std::cout << "[AMateria]: Destructor called\n";
 }
 
 //TODO:
