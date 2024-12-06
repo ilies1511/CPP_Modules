71847aa699b56f2a97feae6ad4db2abb63b478f5
ilies1511 <iziane@student.42heilbronn.de>
2024-12-06 17:51:47 +0100

    FEAT(cpp05, ex02): add RobotomyRequestForm

--- CPP_05/ex02/Includes/RobotomyRequestForm.hpp
+++ CPP_05/ex02/Includes/RobotomyRequestForm.hpp
@@ -1,1 +1,21 @@
 #pragma once
+
+//Includes
+#include "../Includes/AForm.hpp"
+
+//Class
+class RobotomyRequestForm : public AForm
+{
+	private:
+		std::string _target;
+	//OCF
+	public:
+		RobotomyRequestForm(void);
+		RobotomyRequestForm(const std::string& target);
+		RobotomyRequestForm(const RobotomyRequestForm &og);
+		RobotomyRequestForm &operator=(const RobotomyRequestForm &og);
+		~RobotomyRequestForm(void);
+	//Members
+		void execute(Bureaucrat const & executor) const override;
+		std::string	getTarget(void) const;
+};
