4e12a301ca39aad2cdbbd479c3d768337453f8b2
ilies1511 <iziane@student.42heilbronn.de>
2024-12-06 12:59:17 +0100

    FEAT(cpp05, ex02): Add ocf ShrubberyCreationForm

--- CPP_05/ex02/src/ShrubberyCreationForm.cpp
+++ CPP_05/ex02/src/ShrubberyCreationForm.cpp
@@ -1,1 +1,34 @@
 #include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm(void)
+	: AForm("ShrubberyCreationForm", 145, 137), _target("root")
+{
+	printer::ocf_printer("ShrubberyCreationForm", printer::OCF_TYPE::DC);
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target)
+	: AForm("ShrubberyCreationForm", 145, 137), _target(target)
+{
+	printer::ocf_printer("ShrubberyCreationForm", printer::OCF_TYPE::DNC);
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &og)
+	: AForm(og), _target(og._target)
+{
+	printer::ocf_printer("ShrubberyCreationForm", printer::OCF_TYPE::CC);
+}
+
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationForm &og)
+{
+	if (this != &og)
+	{
+		this->_target = og._target;
+		printer::ocf_printer("ShrubberyCreationForm", printer::OCF_TYPE::CAC);
+	}
+	return (*this);
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm(void)
+{
+	printer::ocf_printer("ShrubberyCreationForm", printer::OCF_TYPE::D);
+}
