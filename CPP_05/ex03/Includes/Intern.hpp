c248188347d7e3092ab3e9ec89f18c45603c8160
ilies1511 <iziane@student.42heilbronn.de>
2024-12-07 20:14:55 +0100

    FEAT(cpp05, ex03): Add Intern.[cpp/hpp]

--- CPP_05/ex03/Includes/Intern.hpp
+++ CPP_05/ex03/Includes/Intern.hpp
@@ -4,19 +4,33 @@
 #include <iostream>
 #include <string>
 #include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
 //Class
 
 class Intern
 {
 	private:
+		AForm	*createShrubberyForm(const std::string &target);
+		AForm	*createRobotomyRequestForm(const std::string &target);
+		AForm	*createPresidentialPardonForm(const std::string &target);
 	//OCF
 	public:
 		Intern(void);
-		Intern(const std::string InputName);
 		Intern(const Intern &og);
 		Intern &operator=(const Intern &og);
 		~Intern(void);
 	//Members
 	public:
-		AForm	*makeForm(const std::string NameForm, const std::string TargetOfForm);
+		/*
+			ARGS:
+				First arg: name of a form a,
+				Second arg: target of the form
+			Return Value:
+				Pointer to Form object (whose name is passed as param => first param)
+				That Form Object will initialized its target with second param
+		*/
+		AForm	*makeForm(const std::string &NameForm, const std::string &TargetOfForm);
 };
--- CPP_05/ex03/src/Intern.cpp
+++ CPP_05/ex03/src/Intern.cpp
@@ -1,3 +1,72 @@
 #include "Intern.hpp"
 
+//OCF--BEGIN
+Intern::Intern(void)
+{
+	printer::ocf_printer("Intern", printer::OCF_TYPE::DC);
+}
+
+Intern::Intern(const Intern &og)
+{
+	*this = og;
+	printer::ocf_printer("Intern", printer::OCF_TYPE::CC);
+}
+
+Intern &Intern::operator=(const Intern &og)
+{
+	if (this != &og)
+	{
+		printer::ocf_printer("Intern", printer::OCF_TYPE::CAC);
+		(void)og;
+	}
+	return (*this);
+}
+
+Intern::~Intern(void)
+{
+	printer::ocf_printer("Intern", printer::OCF_TYPE::D);
+}
+//OCF--END
+
+//Members--BEGIN
+AForm	*Intern::makeForm(const std::string &NameForm, const std::string &TargetOfForm)
+{
+	size_t	len;
+	AForm* (Intern::*funcs[])(const std::string& target) = \
+		{
+			&Intern::createShrubberyForm,
+			&Intern::createRobotomyRequestForm,
+			&Intern::createPresidentialPardonForm
+		};
+	std::string Forms[] {"ShrubberyCreationForm", "RobotomyRequestForm", "PresidentialPardonForm"};
+	len = sizeof(Forms) / sizeof(Forms[0]);
+	int i = 0;
+	for (; static_cast<unsigned long>(i) < len; i++)
+	{
+		if (!Forms[i].compare(NameForm))
+		{
+			std::cout << "Intern creates " << NameForm << "\n";
+			return (this->*funcs[i])(TargetOfForm);
+		}
+	}
+	std::cout << coloring("Form" + NameForm + " does not exist\n", RED);
+	return (nullptr);
+}
+
+AForm *Intern::createShrubberyForm(const std::string &target)
+{
+	return (new ShrubberyCreationForm(target));
+}
+
+AForm *Intern::createRobotomyRequestForm(const std::string &target)
+{
+	return (new RobotomyRequestForm(target));
+}
+
+AForm *Intern::createPresidentialPardonForm(const std::string &target)
+{
+	return (new PresidentialPardonForm(target));
+}
+
+//Members--END
 
