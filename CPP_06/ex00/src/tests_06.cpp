0c3b12eaabc00174ac95a350677f44245e940c8b
ilies1511 <iziane@student.42heilbronn.de>
2024-12-09 19:39:22 +0100

    TEST(cpp06, ex00): tests for TypeDetection fncs

--- CPP_06/ex00/src/tests_06.cpp
+++ CPP_06/ex00/src/tests_06.cpp
@@ -6,7 +6,7 @@
 /*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 09:50:36 by iziane            #+#    #+#             */
-/*   Updated: 2024/12/09 18:36:22 by iziane           ###   ########.fr       */
+/*   Updated: 2024/12/09 19:37:37 by iziane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,71 @@
 //IMPLEMENTATION
 namespace testrunner
 {
+	static void checkWhat(const std::string& Type, const std::string& testInput, bool (*checkFunction)(const std::string&))
+	{
+		std::cout << testInput << ": ";
+		if (checkFunction(testInput))
+			std::cout << "is " << Type << "\n";
+		else
+			std::cout << "not " << Type << "\n";
+	}
+
+	static void	checkDouble(void)
+	{
+		printer::Header("\nTEST DOUBLE");
+		checkWhat("double", "a" ,isDouble);
+		checkWhat("double", "aa" ,isDouble);
+		checkWhat("double", "9" ,isDouble);
+		checkWhat("double", "~" ,isDouble);
+		checkWhat("double", "1" ,isDouble);
+		checkWhat("double", "1.0f" ,isDouble);
+		checkWhat("double", "42.000" ,isDouble);
+		checkWhat("double", "-123" ,isDouble);
+		checkWhat("double", "" ,isDouble);
+	}
+
+	static void	checkFloat(void)
+	{
+		printer::Header("\nTEST FLOAT");
+		checkWhat("float", "a" ,isFloat);
+		checkWhat("float", "aa" ,isFloat);
+		checkWhat("float", "\n" ,isFloat);
+		checkWhat("float", "9" ,isFloat);
+		checkWhat("float", "~" ,isFloat);
+		checkWhat("float", "1" ,isFloat);
+		checkWhat("float", "1.0f" ,isFloat);
+		checkWhat("float", "42" ,isFloat);
+		checkWhat("float", "-123" ,isFloat);
+		checkWhat("float", "" ,isFloat);
+	}
+
+	// isInt: "42", "-123", "0", ungültige Fälle: "42.0", "42f".
+	static void	checkInt(void)
+	{
+		printer::Header("\nTEST INT");
+		checkWhat("int", "a" ,isInt);
+		checkWhat("int", "aa" ,isInt);
+		checkWhat("int", "9" ,isInt);
+		checkWhat("int", "~" ,isInt);
+		checkWhat("int", "1" ,isInt);
+	}
+
 	// isChar: 'a', '~', ' ', ungültige Fälle: "ab", "9".
 	static void	checkChar(void)
 	{
-		printer::Header("Test01");
-		std::cout << "a: ";
-		if (isChar("a"))
-			std::cout << "Is Char\n";
-		else
-			std::cout << "No Char\n";
-
-		printer::Header("Test02");
-		std::cout << "ab: ";
-		if (isChar("ab"))
-		std::cout << "Is Char\n";
-		else
-			std::cout << "No Char\n";
-
-		printer::Header("Test03");
-		std::cout << "9: ";
-		if (isChar("9"))
-			std::cout << "Is Char\n";
-		else
-			std::cout << "No Char\n";
+		printer::Header("\nTEST CHAR");
+		checkWhat("char", "a" ,isChar);
+		checkWhat("char", "aa" ,isChar);
+		checkWhat("char", "9" ,isChar);
+		checkWhat("char", "~" ,isChar);
 	}
+
 	void	TypeDetectionTest(void)
 	{
 		checkChar();
-		//TODO:
 		checkInt();
 		checkFloat();
 		checkDouble();
+		// //TODO:
 	}
 } // namespace testrunnner
