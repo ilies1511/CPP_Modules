47ecebc661deac9ece9c59d1e828f8fb601c14c3
ilies1511 <iziane@student.42heilbronn.de>
2024-12-10 00:14:45 +0100

    FEAT(cpp06, ex00): add improved Char TypeDetection

--- CPP_06/ex00/src/TypeDetection.cpp
+++ CPP_06/ex00/src/TypeDetection.cpp
@@ -13,11 +13,64 @@ bool	isPseudoLiteral(const std::string& literal)
 	return (false);
 }
 
-bool	isChar(const std::string& literal)
+// // if (literal.size() > 0 && !isPseudoLiteral(literal))
+// bool	isChar(const std::string& literal)
+// {
+// 	double	res;
+
+// 	char *endptr = nullptr;
+// 	res = std::strtod(literal.c_str(), &endptr);
+// 	if (literal.c_str() == endptr)
+// 	{
+// 		if (literal.length() == 1
+// 			&& !std::isdigit(literal[0]) && !isPseudoLiteral(literal))
+// 			return (true);
+// 	}
+// 	else if (res >= 0 && res <=0)
+// 	{
+// 		return (true);
+// 	}
+// 	return (false);
+// 	 // Regex: Prüft auf genau ein ASCII-Zeichen (sichtbar oder unsichtbar)
+// 	std::regex charRegex(R"(^[\x00-\x7F]$)");
+// 	// Prüfen, ob der String genau ein Zeichen lang ist und auf das Muster passt
+// 	return (std::regex_match(literal, charRegex));
+// }
+
+
+
+bool isValidAsciiValue(const std::string& literal)
 {
-	if (literal.length() == 1 && std::isprint(literal[0]) \
-			&& !std::isdigit(literal[0]) && !isPseudoLiteral(literal))
+	std::regex asciiRegex(R"(^(12[0-7]|1[01][0-9]|[1-9]?[0-9])$)");
+	return (std::regex_match(literal, asciiRegex));
+}
+
+bool isValidQuotedAlphabeticalChar(const std::string& literal)
+{
+	std::regex charRegex(R"(^'[a-zA-Z]'$)");
+
+	return (std::regex_match(literal, charRegex));
+}
+
+bool isChar(const std::string& literal)
+{
+	if (isValidAsciiValue(literal))
 		return (true);
+	if (isValidQuotedAlphabeticalChar(literal))
+		return (true);
+	// double	res;
+	// char *endptr = nullptr;
+	// res = std::strtod(literal.c_str(), &endptr);
+	// if (literal.c_str() == endptr)
+	// {
+	if (literal.length() == 1
+		&& !std::isdigit(literal[0]) && !isPseudoLiteral(literal))
+		return (true);
+	// }
+	// else if (res >= 0 && res <=0)
+	// {
+	// 	return (true);
+	// }
 	return (false);
 }
 
