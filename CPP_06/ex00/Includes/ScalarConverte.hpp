d9e8bad1bd4eff902804204f993f3340703455bd
ilies1511 <iziane@student.42heilbronn.de>
2024-12-09 18:04:52 +0100

    FEAT(cpp06, ex00): Add blueprint for ScalarConverte Class

---
+++ CPP_06/ex00/Includes/ScalarConverte.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+//Includes
+#include <iostream>
+#include <string>
+#include <regex>
+#include <limits>
+#include <exception>
+#include "Extra/printer.hpp"
+#include "Extra/Log.hpp"
+#include "TypeDetection.hpp"
+
+//Class
+class ScalarConverte
+{
+	private:
+	// OCF - Deaktiviert, da Instanzierung nicht erlaubt
+	public:
+		ScalarConverte(void) = delete;
+		ScalarConverte(const ScalarConverte &og) = delete;
+		ScalarConverte operator=(const ScalarConverte &og) = delete;
+		~ScalarConverte(void) = delete;
+	//Exceptions
+	public:
+		class NonDisplayableCharException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		class ImpossibleConversionException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+//Memebers
+	public:
+		static void	convert(const std::string& literal);
+};
---
+++ CPP_06/ex00/src/ScalarConverte.cpp
@@ -0,0 +1,20 @@
+#include "ScalarConverte.hpp"
+
+//Exceptions--BEGIN
+const char *ScalarConverte::NonDisplayableCharException::what() const throw()
+{
+	return ("Non displayable Character !");
+}
+
+const char *ScalarConverte::ImpossibleConversionException::what() const throw()
+{
+	return ("Impossible Conversion !");
+}
+//Exceptions--END
+
+void	ScalarConverte::convert(const std::string& literal)
+{
+	;
+}
+
+
