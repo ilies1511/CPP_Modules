a102313b306a0b2b03ccd1c0501dceb9447a9ae8
ilies1511 <iziane@student.42heilbronn.de>
2024-12-15 15:54:37 +0100

    DOC(cpp06): meaning of static methodes in Class

--- CPP_06/ex00/Includes/ScalarConverte.hpp
+++ CPP_06/ex00/Includes/ScalarConverte.hpp
@@ -6,7 +6,7 @@
 /*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 14:45:38 by iziane            #+#    #+#             */
-/*   Updated: 2024/12/15 15:36:08 by iziane           ###   ########.fr       */
+/*   Updated: 2024/12/15 15:47:43 by iziane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,3 +59,27 @@ void	handlePseudoLiteral(const std::string& literal);
 void	handleChar(const std::string& literal);
 void	handleInt(const std::string& literal);
 void	handleFloat_Double(const std::string& literal);
+
+/*
+ * The 'static' keyword in this context is used to define methods that are
+ * independent of any specific instance of the class. These methods can be
+ * called directly on the class itself, without needing to create an object
+ * of the class.
+ *
+ * In the ScalarConverte class, the methods 'serialize' and 'deserialize'
+ * are declared as 'static' because they provide functionality that is not
+ * tied to a particular instance of the class. Instead, they operate on
+ * input data (like pointers and raw values) and return results.
+ *
+ * This also ensures that:
+ * - No instance of the ScalarConverte class can be created (as the constructor
+ *   is deleted).
+ * - The methods are accessible via the class itself, without the need for
+ *   instantiating an object.
+ * - The methods can be accessed in a simple and clear way, using the class
+ *   name followed by the method name (e.g., ScalarConverte::serialize(...)).
+ *
+ * Using static methods in this case helps organize code that performs specific,
+ * class-wide operations without requiring an object, making the code more
+ * efficient and clearly structured.
+ */
