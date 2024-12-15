e3b7c10cbfa056a23b85f0ebbdf860bb8f9122a2
ilies1511 <iziane@student.42heilbronn.de>
2024-12-15 20:26:51 +0100

    FEAT(cpp07, ex01): Add template - iter + printElement

--- CPP_07/ex01/Includes/iter.hpp
+++ CPP_07/ex01/Includes/iter.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+//Includes
+#include <cstddef>
+
+//Template
+template<typename T, typename FNC>
+void iter(T *array, std::size_t length, FNC func)
+{
+	for (size_t i = 0; i < length; i++)
+		func(array[i]);
+	return ;
+}
+
+//TestTemplates
+template <typename T>
+void printElement(const T& element)
+{
+	std::cout << element << std::endl;
+}
