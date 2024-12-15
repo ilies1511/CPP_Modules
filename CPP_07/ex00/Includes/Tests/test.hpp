0e9020b2d46717d36652e10015d78c55f4182270
ilies1511 <iziane@student.42heilbronn.de>
2024-12-15 16:16:27 +0100

    INIT(cpp07, ex00)

---
+++ CPP_07/ex00/Includes/Tests/test.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 21:34:14 by iziane            #+#    #+#             */
+/*   Updated: 2024/12/15 16:12:12 by iziane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+//Includes
+#include "iostream"
+#include "string"
+#include <cstdlib>
+#include <ctime>
+//Mine
+#include "printer.hpp"
+#include "Log.hpp"
+
+//Project
+
+namespace testrunner
+{
+	;
+}
+
---
+++ CPP_07/ex00/src/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 14:47:28 by iziane            #+#    #+#             */
+/*   Updated: 2024/12/15 16:13:35 by iziane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Includes/Tests/test.hpp"
+
+int	main(void)
+{
+	/*
+		Leaks Check:
+			dorker valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./ex03.out
+	*/
+	printer::Header("TESTS");
+	return (0);
+}
