e4ab67524db887f64cfdde3a8976400ac0e6e706
ilies1511 <iziane@student.42heilbronn.de>
2024-12-15 19:31:13 +0100

    FEAT(cpp07, ex00): Add template fncs (min, max, swap)

--- CPP_07/ex00/Includes/whatever.hpp
+++ CPP_07/ex00/Includes/whatever.hpp
@@ -1,3 +1,36 @@
 #pragma once
 
+/*
+	swap: Swaps the values of two given arguments. Does not return anything.
+*/
+template<typename T>
+void swap(T &variable1, T &variable2)
+{
+	T	temp;
+
+	temp = variable1;
+	variable1 = variable2;
+	variable2 = temp;
+	return ;
+}
+
+/*
+	min: Compares the two values passed in its arguments and returns the
+	smallest one. If the two of them are equal, then it returns the second one.
+*/
+template<typename T>
+T min(const T &variable1, const T &variable2)
+{
+	return ((variable2 <= variable1) ? variable2 : variable1);
+}
+
+/*
+	max: Compares the two values passed in its arguments and returns the
+	greatest one. If the two of them are equal, then it returns the second one.
+*/
+template<typename T>
+T max(const T &variable1, const T &variable2)
+{
+	return ((variable2 >= variable1) ? variable2 : variable1);
+}
 
