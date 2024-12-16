c372231b56940bdf4cd4f47e00fd1233e3d53608
ilies1511 <iziane@student.42heilbronn.de>
2024-12-16 14:56:20 +0100

    FEAT(cpp07, ex02): Add blueprint of class template Array

--- CPP_07/ex02/Includes/Array.hpp
+++ CPP_07/ex02/Includes/Array.hpp
@@ -1,3 +1,43 @@
 #pragma once
 
+//Includes
+#include <iostream>
+#include <string>
+#include "printer.hpp"
 
+template<typename T>
+class Array
+{
+	private:
+		T		*_array; // Pointer to Array
+		size_t	_size; // Amount of Elements in Array
+	//OCF
+	public:
+		Array(void);
+		Array(size_t n);
+		Array(const Array &og);
+		Array &operator=(const Array &og);
+		~Array(void);
+	//Methodes
+	public:
+		size_t	size(void) const;
+	//Overload
+	public:
+		/*
+			Overloeds '[]' operator in two ways:
+				1. Returns reference from element --> enables its modification (write)
+				2. Returns reference from element but CONST protect from modification --> enables its modification (only read)
+		*/
+		T& operator[](size_t index); //1
+		const T& operator[](size_t index) const; //2
+};
+	/*
+		Bedeutung der Const: const T& operator[](size_t index) const
+			Erstes const:	Die zurückgegebene Referenz (const T&) ist konstant
+							und kann nicht geändert werden.
+			Zweites const:	Die Methode selbst darf keine Member-Variablen der
+							Klasse ändern und kann deshalb auf const-Objekten
+							aufgerufen werden.
+
+	*/
+#include "Array.tpp"
