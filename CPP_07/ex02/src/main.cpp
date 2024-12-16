4146b748544e2553a90e66b16fcb9c0e60207b01
ilies1511 <iziane@student.42heilbronn.de>
2024-12-16 16:29:31 +0100

    FEAT(cpp07, ex02): Add main.cpp & main_subj.cpp (provided)

---
+++ CPP_07/ex02/src/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 14:47:28 by iziane            #+#    #+#             */
+/*   Updated: 2024/12/16 13:18:56 by iziane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Includes/Tests/test.hpp"
+
+int	main(void)
+{
+	/*
+		Leaks Check:
+			dorker valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./ex03.out
+	*/
+
+	printer::Header("TESTS CPP07 EX02");
+	return (0);
+}
