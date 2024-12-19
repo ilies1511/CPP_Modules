1657bbcd0b860a70a02098af08585966f50769a8
ilies1511 <iziane@student.42heilbronn.de>
2024-12-19 22:45:14 +0100

    TEST(cpp08, ex00): Add test to testrunner

--- CPP_08/ex00/src/Tests/test.cpp
+++ CPP_08/ex00/src/Tests/test.cpp
@@ -6,7 +6,7 @@
 /*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 09:50:36 by iziane            #+#    #+#             */
-/*   Updated: 2024/12/16 23:41:38 by iziane           ###   ########.fr       */
+/*   Updated: 2024/12/19 22:44:09 by iziane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,22 @@
 //IMPLEMENTATION
 namespace testrunner
 {
-	;
+	void	ex00(void)
+	{
+		try
+		{
+			std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+			auto it = easyfind(numbers, -12);
+			std::cout << "Found: " << *it << std::endl;
+
+			// Test case: Value not found
+			easyfind(numbers, 10); // Throws NotFoundException
+		}
+		catch (const std::exception& e)
+		{
+			printer::LogException(e, __FILE__, __FUNCTION__, __LINE__);
+			std::cerr << e.what() << std::endl;
+		}
+	}
 } // namespace testrunnner
