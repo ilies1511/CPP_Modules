2165c842f7e53ca9b1bd9cab5eb65fa6f4dc4847
ilies1511 <iziane@student.42heilbronn.de>
2024-12-19 22:55:31 +0100

    TEST(cpp08, ex00): Add vector tests

--- CPP_08/ex00/src/Tests/test.cpp
+++ CPP_08/ex00/src/Tests/test.cpp
@@ -6,7 +6,7 @@
 /*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 09:50:36 by iziane            #+#    #+#             */
-/*   Updated: 2024/12/19 22:44:09 by iziane           ###   ########.fr       */
+/*   Updated: 2024/12/19 22:54:27 by iziane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,44 @@ namespace testrunner
 {
 	void	ex00(void)
 	{
+		printer::Header("TEST: Vector<int>");
 		try
 		{
 			std::vector<int> numbers = {1, 2, 3, 4, 5};
 
+			// std::vector<int>::iterator it = easyfind(numbers, -12);
 			auto it = easyfind(numbers, -12);
 			std::cout << "Found: " << *it << std::endl;
-
-			// Test case: Value not found
-			easyfind(numbers, 10); // Throws NotFoundException
 		}
 		catch (const std::exception& e)
 		{
 			printer::LogException(e, __FILE__, __FUNCTION__, __LINE__);
-			std::cerr << e.what() << std::endl;
+		}
+		printer::Header("\nTEST: Vector<int>");
+		try
+		{
+			std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+			// std::vector<int>::iterator it = easyfind(numbers, -12);
+			auto it = easyfind(numbers, 1);
+			std::cout << coloring("Found: " + std::to_string(*it) + "\n", GREEN);
+		}
+		catch (const std::exception& e)
+		{
+			printer::LogException(e, __FILE__, __FUNCTION__, __LINE__);
+		}
+		printer::Header("\nTEST: Vector<char>");
+		try
+		{
+			std::vector<char> numbers = {'1', '2', '3', '4', '5'};
+
+			// std::vector<int>::iterator it = easyfind(numbers, -12);
+			auto it = easyfind(numbers, '1');
+			std::cout << coloring("Found: " + std::to_string(*it) + "\n", GREEN);
+		}
+		catch (const std::exception& e)
+		{
+			printer::LogException(e, __FILE__, __FUNCTION__, __LINE__);
 		}
 	}
 } // namespace testrunnner
