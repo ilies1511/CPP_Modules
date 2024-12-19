26923b3fa2679fae62dc5f62ef082581435e17f8
ilies1511 <iziane@student.42heilbronn.de>
2024-12-19 22:40:01 +0100

    FEAT(cpp08, ex00): Add file structure

--- CPP_08/ex00/Includes/easyfind.hpp
+++ CPP_08/ex00/Includes/easyfind.hpp
@@ -1,6 +1,71 @@
+/*
+Summary of Templates and Iterators in C++:
+
+1. **What is a Container?**
+	- A container is a data type that stores multiple values (e.g.,
+	`std::vector`, `std::list`).
+	- Example: A `std::vector<int>` holds a list of integers.
+
+2. **What is an Iterator?**
+
+	- An iterator is a special "pointer" that points to an element in a container.
+
+	- Containers like `std::vector` and `std::list` have their own iterator types,
+	e.g., `std::vector<int>::iterator`.
+
+3. **What is a Template?**
+	- Templates make functions and classes flexible,
+	allowing them to work with different types (e.g.,
+	different containers) without rewriting the code.
+	- Example: A template function can work with both a `std::vector` and a `std::list`.
+
+4. **How to Search in Containers?**
+
+	- The `std::find` function searches for a value in a container and returns an iterator to the found element.
+	- If the value is not found, the iterator points to the end of the container.
+
+5. **How to Implement a Generic Search Function (`easyfind`)?**
+
+	- Write a template function using `typename T` to make it adaptable for different containers.
+	- Use `std::find` to search for a value.
+	- Throw an exception if the value is not found.
+
+	**Example Code:**
+	```cpp
+	template <typename T>
+	typename T::iterator easyfind(T& container, int value) {
+		typename T::iterator it = std::find(container.begin(), container.end(),
+				value);
+		if (it == container.end()) {
+			throw std::runtime_error("Value not found");
+		}
+		return (it);
+	}
+*/
+
 #pragma once
 
+// Includes --BEGIN
+#include <algorithm>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <stdexcept>
+#include <string>
+#include <vector>
+// Includes --END
+
+//Exception--BEGIN
+class NotFoundException : public std::exception
+{
+	public:
+		virtual const char *what() const throw();
+};
+//Exception--END
 
 
+//Function Template
+template <typename T> typename T::iterator easyfind(T &container, int needle);
 
 #include "easyfind.tpp"
--- CPP_08/ex00/Includes/easyfind.tpp
+++ CPP_08/ex00/Includes/easyfind.tpp
@@ -1,1 +1,14 @@
 #include "easyfind.hpp"
+
+/*
+	Definition of Function Template
+*/
+template <typename T> typename T::iterator easyfind(T &container, int needle)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), // 'typename T::iterator it' same as 'auto it'
+			needle);
+	if (it == container.end())
+		throw NotFoundException();
+			// throw std::runtime_error("Value not found");
+	return (it);
+}
