9cd4132c1c311d02fd4dfee0927139f963d77eb5
ilies1511 <iziane@student.42heilbronn.de>
2024-12-19 22:41:45 +0100

    TEST(cpp08, ex00): Add first test

--- CPP_08/ex00/Includes/Tests/test.hpp
+++ CPP_08/ex00/Includes/Tests/test.hpp
@@ -6,22 +6,33 @@
 /*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 21:34:14 by iziane            #+#    #+#             */
-/*   Updated: 2024/12/16 23:41:08 by iziane           ###   ########.fr       */
+/*   Updated: 2024/12/17 19:14:14 by iziane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 //Includes
-#include "iostream"
-#include "string"
+#include <iostream>
+#include <string>
 #include <cstdlib>
 #include <ctime>
+//Container-Specefic
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+
 //Mine
 #include "printer.hpp"
 #include "Log.hpp"
 
 //Project
+#include "easyfind.hpp"
 
 namespace testrunner
 {
--- CPP_08/ex00/src/main.cpp
+++ CPP_08/ex00/src/main.cpp
@@ -6,11 +6,12 @@
 /*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 14:47:28 by iziane            #+#    #+#             */
-/*   Updated: 2024/12/16 23:41:43 by iziane           ###   ########.fr       */
+/*   Updated: 2024/12/19 22:40:41 by iziane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/Tests/test.hpp"
+#include "../Includes/Extra/printer.hpp"
 
 int	main(void)
 {
@@ -19,5 +20,21 @@ int	main(void)
 			dorker valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./ex03.out
 	*/
 	printer::Header("TESTS CPP08");
+
+	try
+	{
+		std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+		auto it = easyfind(numbers, -12);
+		std::cout << "Found: " << *it << std::endl;
+
+		// Test case: Value not found
+		easyfind(numbers, 10); // Throws NotFoundException
+	}
+	catch (const std::exception& e)
+	{
+		printer::LogException(e, __FILE__, __FUNCTION__, __LINE__);
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
