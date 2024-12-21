4d9140548914f6ebaa6cc4127cbde035d72396d1
ilies1511 <iziane@student.42heilbronn.de>
2024-12-21 20:42:41 +0100

    DOCS(cpp08, ex01)

--- CPP_08/ex01/Includes/Span.hpp
+++ CPP_08/ex01/Includes/Span.hpp
@@ -17,7 +17,7 @@ class Span
 {
 	private:
 		std::vector<int>	_numbers;
-		unsigned int		_maxSize;
+		unsigned int		_maxSize; //the maximum capacity
 	//OCF
 	public:
 		Span(void);
@@ -33,6 +33,7 @@ class Span
 		template <typename Iterator>
 		void	addRange(Iterator begin, Iterator end)
 		{
+			//Check for capacity
 			if (std::distance(begin, end) + _numbers.size() > _maxSize)
 			{
 				throw (std::runtime_error("Container size is too small to add the range!"));
@@ -40,3 +41,13 @@ class Span
 			_numbers.insert(_numbers.end(), begin, end);
 		}
 };
+
+
+/*
+	iterator insert(iterator pos, InputIterator first, InputIterator last):
+		- pos: The position in the vector where new elements will be inserted.
+		- first and last: Iterators specifying the range of elements to insert.
+		In der Funktion fügt _numbers.insert(_numbers.end(), begin, end)
+		alle Elemente, die im Bereich von begin bis end liegen, hinter dem
+		letzten Element des Containers _numbers ein.
+*/
