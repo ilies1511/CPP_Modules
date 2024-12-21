e572a13e0f97cea5b53a15734fe138dc0ece64e4
ilies1511 <iziane@student.42heilbronn.de>
2024-12-21 01:17:27 +0100

    FEAT(cpp08, ex01): add headerfile

--- CPP_08/ex01/Includes/Span.hpp
+++ CPP_08/ex01/Includes/Span.hpp
@@ -9,4 +9,34 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <limits>
 // Includes --END
+
+//Class
+class Span
+{
+	private:
+		std::vector<int>	_numbers;
+		unsigned int		_maxSize;
+	//OCF
+	public:
+		Span(void);
+		Span(unsigned int N);
+		Span(const Span &og);
+		Span &operator=(const Span &og);
+		~Span(void);
+	//Members
+		void	addNumber(size_t Input);
+		size_t	shortestSpan(void);
+		size_t	longestSpan(void);
+		//Template fnc
+		template <typename Iterator>
+		void	addRange(Iterator begin, Iterator end)
+		{
+			if (std::distance(begin, last) + _numbers.size() > _maxSize)
+			{
+				throw (std::runtime_error("Containe Size is to small !"));
+			}
+			_numbers.insert(_numbers.end(), begin, end);
+		}
+};
