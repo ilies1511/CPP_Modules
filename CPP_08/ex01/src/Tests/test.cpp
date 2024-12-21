1b16df4a882238c84a88dd3500aed567fefabb4c
ilies1511 <iziane@student.42heilbronn.de>
2024-12-21 22:27:18 +0100

    TEST(cpp08, ex01): Add subj Test

--- CPP_08/ex01/src/Tests/test.cpp
+++ CPP_08/ex01/src/Tests/test.cpp
@@ -6,15 +6,34 @@
 /*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 09:50:36 by iziane            #+#    #+#             */
-/*   Updated: 2024/12/19 23:30:06 by iziane           ###   ########.fr       */
+/*   Updated: 2024/12/21 22:26:28 by iziane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/Tests/test.hpp"
+#include "Span.hpp"
 // #include "test.hpp"
 
 //IMPLEMENTATION
 namespace testrunner
 {
-	;
+	int	ex01_subj(void)
+	{
+		Span	sp = Span(5);
+
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+		if (sp.shortestSpan() != 2)
+			return (EXIT_FAILURE);
+		if (sp.longestSpan() != 14)
+			return (EXIT_FAILURE);
+		std::cout << coloring(std::string("Function '") + __FUNCTION__ + "()' passed all the tests", GREEN) << "\n";
+		// std::cout << coloring(std::string("Function: ") + __FUNCTION__, GREEN) << std::endl;
+		return (EXIT_SUCCESS);
+	}
 } // namespace testrunnner
--- CPP_08/ex01/src/main.cpp
+++ CPP_08/ex01/src/main.cpp
@@ -6,12 +6,13 @@
 /*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 14:47:28 by iziane            #+#    #+#             */
-/*   Updated: 2024/12/19 23:31:00 by iziane           ###   ########.fr       */
+/*   Updated: 2024/12/21 22:19:33 by iziane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/Tests/test.hpp"
 #include "../Includes/Extra/printer.hpp"
+#include "Span.hpp"
 
 // cp -r ex00 ex01
 int	main(void)
@@ -21,5 +22,6 @@ int	main(void)
 			dorker valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./ex03.out
 	*/
 	printer::Header("TESTS CPP08 EX01");
+	testrunner::ex01_subj();
 	return (0);
 }
