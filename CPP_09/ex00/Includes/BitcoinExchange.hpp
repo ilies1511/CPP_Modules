ae92004b0254bfa4b3e4e1c520b099286cbb5902
ilies1511 <iziane@student.42heilbronn.de>
2024-12-25 15:12:00 +0100

    CHORE(cpp09, ex00): add frame

---
+++ CPP_09/ex00/Includes/BitcoinExchange.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+//Includes
+#include <algorithm>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <limits>
+#include <deque>
+#include <stack>
+#include <map>
+//Own
+#include "printer.hpp"
+
+//Class
+
+
--- CPP_09/ex00/src/main.cpp
+++ CPP_09/ex00/src/main.cpp
@@ -6,19 +6,22 @@
 /*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 14:47:28 by iziane            #+#    #+#             */
-/*   Updated: 2024/12/25 14:50:10 by iziane           ###   ########.fr       */
+/*   Updated: 2024/12/25 15:08:36 by iziane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/Tests/test.hpp"
 #include "../Includes/Extra/printer.hpp"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	/*
 		Leaks Check:
 			dorker valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./ex03.out
 	*/
+
+	(void)argc;
+	(void)argv;
 	printer::Header("TESTS CPP09");
 	return (0);
 }
