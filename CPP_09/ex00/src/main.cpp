549ef174d3ed411a618e17773b82b8d405f64d12
ilies1511 <iziane@student.42heilbronn.de>
2024-12-25 14:54:09 +0100

    INIT(cpp09, ex00): initial commit

---
+++ CPP_09/ex00/src/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iziane <iziane@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 14:47:28 by iziane            #+#    #+#             */
+/*   Updated: 2024/12/25 14:50:10 by iziane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Includes/Tests/test.hpp"
+#include "../Includes/Extra/printer.hpp"
+
+int	main(void)
+{
+	/*
+		Leaks Check:
+			dorker valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./ex03.out
+	*/
+	printer::Header("TESTS CPP09");
+	return (0);
+}
