7ca2bc230ddbd58da7c9afd6e14365c309604b03
ilies1511 <iziane@student.42heilbronn.de>
2025-01-02 23:08:30 +0100

    CHORE(cpp09, ex02): Add rudimentary structure in .hpp file

--- CPP_09/ex02/Includes/PmergeMe.hpp
+++ CPP_09/ex02/Includes/PmergeMe.hpp
@@ -1,26 +1,42 @@
 #pragma once
 
 //Includes
+	//Project-Specific Containers -- BEGIN
+#include <deque>
+#include <vector>
+	//Project-Specific Containers -- END
 #include <iomanip> //Precision
 #include <stdbool.h> //Bool
+	//Parsing -- BEGIN
+#include <sstream>
+#include <fstream>
 #include <limits>
+#include <regex>
+	//Parsing -- END
+#include <cstdlib>
 #include <ctime>
 #include <algorithm>
-#include <cstdlib>
-#include <deque>
 #include <iostream>
-#include <list>
-#include <stdexcept>
 #include <string>
-#include <vector>
-#include <array>
-#include <deque>
-#include <stack>
-#include <fstream>
-#include <sstream>
-#include <cstdlib>
-#include <regex>
-#include <unordered_set>
+	//Exception
+#include <stdexcept>
 
 //Own
 #include "printer.hpp"
+
+
+class PmergeMe
+{
+	private:
+		std::vector<int> _vector;
+		std::deque<int> _deque;
+	//OCF
+	public:
+		PmergeMe(void);
+		PmergeMe(const PmergeMe &og);
+		PmergeMe &operator=(const PmergeMe &og);
+		~PmergeMe(void);
+	//Parser
+	public:
+		bool checkInput(int argc, char **argv);
+};
