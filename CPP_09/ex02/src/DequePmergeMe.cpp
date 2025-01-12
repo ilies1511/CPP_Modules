06291f2082bdf2924e8df9eba7db5b2044583347
ilies1511 <iziane@student.42heilbronn.de>
2025-01-12 21:22:01 +0100

    FEAT(cpp09, ex02): add init_main&pend_chain() function

--- CPP_09/ex02/src/DequePmergeMe.cpp
+++ CPP_09/ex02/src/DequePmergeMe.cpp
@@ -106,5 +106,67 @@ void DequePmergeMe::sort(void)
 	// std::cout << "RdcursionsLEvel: " << pair_level << "\n";
 	this->printContainer();
 	sort(); //Rekursive Call (davor: sort(_container, pair_level * 2))
+	/*TODO: here comes the Inertion Part:
+		1. Initialize main & pend datastrucure (holding Iterators of jeweilige a und b Elemente)
+		2. Insertion:
+			2.1: Check wether pend elements higher than 3 --> yes (Insort(binary Insort) with Index of Jacobsthal Sequence), no (yes but without Jacobsthal)
+			2.2: Init JacobsThal Container (en fonction du nombre absolue de la sequence)
+				(argc - 1) / 2
+				(22 - 1) / 2 = 11 --> wir brauchen bis einschließlich Jacobsthal nbr 11
+			2.2: Insort Rest of Pend Elements (for example {b3, b2}, {b5, b4} --> always in the range of the current - prev Jacobsthal Sequence)
+	*/
+	std::deque<Iterator> main_chain;
+	std::deque<Iterator> pend_chain;
+
+	// main_chain.insert(startpunkt, von (z.b Container a.begin), bis((z.b Container a.end))); --> Inserten einer Range ab Startpunkt
+	// main_chain.insert(startpunkt, das_element); --> Inserten eines einzelnen Elements
+
+	printer::Header("In Insertion Part");
+	this->printContainer();
+	// /*
+	// 	Init main-chain with {b1, a1};
+	// */
+	// main_chain.insert(main_chain.end(), next(_container.begin(), pair_level - 1)); // Inserte den Iterator in main_chain (Iterator zeigt gerade auf letztes Element des Paares) --> b1
+	// main_chain.insert(main_chain.end(), next(_container.begin(), (pair_level * 2) - 1)); // Inserte den Iterator in main_chain (Iterator zeigt gerade auf letztes Element des Paares) --> a1
+
+	// std::cout << "Print Main-Chain 1: \n";
+	// this->printContainerHoldingIterators(main_chain);
+
+	// //Do Rest
+	// for (int i = 4; i <= pair_units_nbr; i += 2)
+	// {
+	// 	pend_chain.insert(pend_chain.end(), next(_container.begin(), pair_level * (i - 1) - 1));
+	// 	main_chain.insert(main_chain.end(), next(_container.begin(), pair_level * i - 1));
+	// }
+
+
+	printer::Header("Init MAIN -& PEND-CHAIN");
+	int pair_index = 2; // Startindex für die Paare (entspricht 2. und 4. Element)
+	do
+	{
+		if (pair_index == 2)
+		{
+			// Initialisierung der main_chain mit den ersten beiden Elementen (b1, a1)
+			main_chain.insert(main_chain.end(), next(_container.begin(), pair_level - 1)); // b1
+			main_chain.insert(main_chain.end(), next(_container.begin(), (pair_level * 2) - 1)); // a1
+			// std::cout << "Print Main-Chain 1: \n";
+		}
+		else
+		{
+			pend_chain.insert(pend_chain.end(), next(_container.begin(), pair_level * (pair_index - 1) - 1));
+			main_chain.insert(main_chain.end(), next(_container.begin(), pair_level * pair_index - 1));
+		}
+		pair_index += 2; // Erhöhe den Index um 2, um das nächste Paar zu verarbeiten
+		// printer::Header("Main-Chain");
+		// this->printContainerHoldingIterators(main_chain);
+		// printer::Header("Pend-Chain");
+		// this->printContainerHoldingIterators(pend_chain);
+	}
+	while (pair_index <= pair_units_nbr);
+	printer::Header("Main-Chain");
+	this->printContainerHoldingIterators(main_chain);
+	printer::Header("Pend-Chain");
+	this->printContainerHoldingIterators(pend_chain);
+	// exit(1);
 }
 //Sort -- END
