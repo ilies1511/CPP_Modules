dd22e250925d89ac7358b354a6086f5e516b3d16
ilies1511 <iziane@student.42heilbronn.de>
2025-01-13 16:50:33 +0100

    CHORE(cpp09, ex02): clean-up sort() --> entry point for sorting algo

--- CPP_09/ex02/src/DequePmergeMe.cpp
+++ CPP_09/ex02/src/DequePmergeMe.cpp
@@ -1,12 +1,12 @@
 #include "DequePmergeMe.hpp"
 
 //OCF -- BEGIN
-DequePmergeMe::DequePmergeMe(int argc, char **argv) : PmergeMe(argc, argv)
+DequePmergeMe::DequePmergeMe(int argc, char **argv) : PmergeMe(argc, argv)//, chains{0, 0, {}, {}, {}, {}, {}, {}, {}}
 {
 	printer::ocf_printer("DequePmergeMe", printer::OCF_TYPE::DC);
 }
 
-DequePmergeMe::DequePmergeMe(const DequePmergeMe &og) : PmergeMe(og)
+DequePmergeMe::DequePmergeMe(const DequePmergeMe &og) : PmergeMe(og)//, chains{0, 0, {}, {}, {}, {}, {}, {}, {}}
 {
 	printer::ocf_printer("DequePmergeMe", printer::OCF_TYPE::CC);
 }
@@ -17,6 +17,7 @@ DequePmergeMe &DequePmergeMe::operator=(const DequePmergeMe &og)
 	{
 		printer::ocf_printer("DequePmergeMe", printer::OCF_TYPE::CAC);
 		PmergeMe::operator=(og);
+		// this->chains = og.chains;
 	}
 	return (*this);
 }
@@ -25,39 +26,6 @@ DequePmergeMe::~DequePmergeMe(void)
 {
 	printer::ocf_printer("DequePmergeMe", printer::OCF_TYPE::D);
 }
-//OCF -- END
-
-
-//Sort -- BEGIN
-int	cal_interPairSize(int recursion_level)
-{
-	return (static_cast<int>(std::pow(2, recursion_level - 1)));
-}
-
-void	go2nextXPair(typename std::deque<int>::iterator it, int interPairSize)
-{
-	std::advance(it, interPairSize);
-}
-
-void	DequePmergeMe::swap_pair(typename std::deque<int>::iterator it, int pair_level)
-{
-	Iterator	start;
-	Iterator	end;
-
-	start = next(it, -pair_level + 1);
-	end = next(start, pair_level);
-	while (start != end)
-	{
-		std::iter_swap(start, next(start, pair_level));
-		++start;
-	}
-}
-
-typename std::deque<int>::iterator next(typename std::deque<int>::iterator it, int steps)
-{
-	std::advance(it, steps);
-	return (it);
-}
 
 template <typename T>
 bool _comp(T lv, T rv)
@@ -65,68 +33,11 @@ bool _comp(T lv, T rv)
 	return (*lv < *rv);
 }
 
-void	DequePmergeMe::init_main_and_pendChain(std::deque<Iterator> &main_chain, std::deque<Iterator> &pend_chain, int &pair_level, int &pair_units_nbr)
-{
-	// std::deque<Iterator> main_chain;
-	// std::deque<Iterator> pend_chain;
-
-	// main_chain.insert(startpunkt, von (z.b Container a.begin), bis((z.b Container a.end))); --> Inserten einer Range ab Startpunkt
-	// main_chain.insert(startpunkt, das_element); --> Inserten eines einzelnen Elements
-
-	printer::Header("In Insertion Part");
-	this->printContainer();
-	// /*
-	// 	Init main-chain with {b1, a1};
-	// */
-	// main_chain.insert(main_chain.end(), next(_container.begin(), pair_level - 1)); // Inserte den Iterator in main_chain (Iterator zeigt gerade auf letztes Element des Paares) --> b1
-	// main_chain.insert(main_chain.end(), next(_container.begin(), (pair_level * 2) - 1)); // Inserte den Iterator in main_chain (Iterator zeigt gerade auf letztes Element des Paares) --> a1
-
-	// std::cout << "Print Main-Chain 1: \n";
-	// this->printContainerHoldingIterators(main_chain);
-
-	// //Do Rest
-	// for (int i = 4; i <= pair_units_nbr; i += 2)
-	// {
-	// 	pend_chain.insert(pend_chain.end(), next(_container.begin(), pair_level * (i - 1) - 1));
-	// 	main_chain.insert(main_chain.end(), next(_container.begin(), pair_level * i - 1));
-	// }
-
-
-	printer::Header("Init MAIN -& PEND-CHAIN");
-	int pair_index = 2; // Startindex für die Paare (entspricht 2. und 4. Element)
-	do
-	{
-		if (pair_index == 2)
-		{
-			// Initialisierung der main_chain mit den ersten beiden Elementen (b1, a1)
-			main_chain.insert(main_chain.end(), next(_container.begin(), pair_level - 1)); // b1
-			main_chain.insert(main_chain.end(), next(_container.begin(), (pair_level * 2) - 1)); // a1
-			// std::cout << "Print Main-Chain 1: \n";
-		}
-		else
-		{
-			pend_chain.insert(pend_chain.end(), next(_container.begin(), pair_level * (pair_index - 1) - 1));
-			main_chain.insert(main_chain.end(), next(_container.begin(), pair_level * pair_index - 1));
-		}
-		pair_index += 2; // Erhöhe den Index um 2, um das nächste Paar zu verarbeiten
-		// printer::Header("Main-Chain");
-		// this->printContainerHoldingIterators(main_chain);
-		// printer::Header("Pend-Chain");
-		// this->printContainerHoldingIterators(pend_chain);
-	}
-	while (pair_index <= pair_units_nbr);
-	printer::Header("Main-Chain");
-	this->printContainerHoldingIterators(main_chain);
-	printer::Header("Pend-Chain");
-	this->printContainerHoldingIterators(pend_chain);
-}
-
-
+//OCF -- END
 void DequePmergeMe::sort(void)
 {
 	int			pair_units_nbr; //Amount of Pairs
 	bool		is_odd;
-	// int			interPairSize;
 	int			next_pairs; //Steps to go until next pair
 	Iterator	start, last, very_last, current_pair, next_pair;
 	int			pair_level;
@@ -154,14 +65,10 @@ void DequePmergeMe::sort(void)
 	// std::cout << "Next_Pairs Rcursion Level" << next_pairs << "\n";
 	for (Iterator it = start; it != very_last; std::advance(it, next_pairs), i++)
 	{
-		// std::cout << "In For Loop round: " << i << "\n";
 		current_pair = next(it, pair_level - 1);
-		// std::cout << "Post next call 1\n";
 		next_pair = next(it, pair_level * 2 - 1);
-		// std::cout << "Post next call 2\n";
 		if (*current_pair > *next_pair)
 		{
-			// std::cout << "In if Condition\n";
 			swap_pair(current_pair, pair_level);
 		}
 	}
@@ -181,55 +88,26 @@ void DequePmergeMe::sort(void)
 	std::deque<Iterator> main_chain;
 	std::deque<Iterator> pend_chain;
 
-	// main_chain.insert(startpunkt, von (z.b Container a.begin), bis((z.b Container a.end))); --> Inserten einer Range ab Startpunkt
-	// main_chain.insert(startpunkt, das_element); --> Inserten eines einzelnen Elements
-
 	printer::Header("In Insertion Part");
 	this->printContainer();
-	// /*
-	// 	Init main-chain with {b1, a1};
-	// */
-	// main_chain.insert(main_chain.end(), next(_container.begin(), pair_level - 1)); // Inserte den Iterator in main_chain (Iterator zeigt gerade auf letztes Element des Paares) --> b1
-	// main_chain.insert(main_chain.end(), next(_container.begin(), (pair_level * 2) - 1)); // Inserte den Iterator in main_chain (Iterator zeigt gerade auf letztes Element des Paares) --> a1
 
-	// std::cout << "Print Main-Chain 1: \n";
-	// this->printContainerHoldingIterators(main_chain);
-
-	// //Do Rest
-	// for (int i = 4; i <= pair_units_nbr; i += 2)
-	// {
-	// 	pend_chain.insert(pend_chain.end(), next(_container.begin(), pair_level * (i - 1) - 1));
-	// 	main_chain.insert(main_chain.end(), next(_container.begin(), pair_level * i - 1));
-	// }
-
-
-	printer::Header("Init MAIN -& PEND-CHAIN");
-	int pair_index = 2; // Startindex für die Paare (entspricht 2. und 4. Element)
-	do
+	this->init_main_and_pendChain(main_chain, pend_chain, pair_level, pair_units_nbr);
+	this->jacobsthal_based_insertion(main_chain, pend_chain);
+	this->sequential_based_insertion(main_chain, pend_chain);
+	if (is_odd)
 	{
-		if (pair_index == 2)
-		{
-			// Initialisierung der main_chain mit den ersten beiden Elementen (b1, a1)
-			main_chain.insert(main_chain.end(), next(_container.begin(), pair_level - 1)); // b1
-			main_chain.insert(main_chain.end(), next(_container.begin(), (pair_level * 2) - 1)); // a1
-			// std::cout << "Print Main-Chain 1: \n";
-		}
-		else
-		{
-			pend_chain.insert(pend_chain.end(), next(_container.begin(), pair_level * (pair_index - 1) - 1));
-			main_chain.insert(main_chain.end(), next(_container.begin(), pair_level * pair_index - 1));
-		}
-		pair_index += 2; // Erhöhe den Index um 2, um das nächste Paar zu verarbeiten
-		// printer::Header("Main-Chain");
-		// this->printContainerHoldingIterators(main_chain);
-		// printer::Header("Pend-Chain");
-		// this->printContainerHoldingIterators(pend_chain);
+		auto odd_pair = next(very_last, pair_level - 1);
+		auto idx = std::upper_bound(main_chain.begin(), main_chain.end(), odd_pair, _comp<Iterator>);
+		main_chain.insert(idx, odd_pair);
 	}
-	while (pair_index <= pair_units_nbr);
-	printer::Header("Main-Chain");
-	this->printContainerHoldingIterators(main_chain);
-	printer::Header("Pend-Chain");
-	this->printContainerHoldingIterators(pend_chain);
+	printer::Header("PRE COPY");
+	this->printContainer();
+
+	this->reconstructContainer(main_chain, pair_level);
+
+	printer::Header("POST reconstructContainer");
+	this->printContainer();
+
 	// exit(1);
 }
 //Sort -- END
